b237299b53eecad274f30eb108dfbe0dba460883
Sandeep Mohan <sjmohan@gmail.com>
2022-09-10 00:00:41 -0400

    finished funda 1

--- 01-Fundamentals-Part-1/starter/script2.js
+++ 01-Fundamentals-Part-1/starter/script2.js
@@ -36,12 +36,49 @@ if (koalaAverageTD === dolphinsAverageTD) {
 if ((koalaAverageTD > dolphinsAverageTD) && (koalaAverageTD > 100)) {
     console.log(`The Koalas Average is: ${koalaAverageTD}
     The Dolphins Average is: ${dolphinsAverageTD}
-    The Koala's Win!!`)
+    The Koala's Win!!`);
 } else if ((koalaAverageTD < dolphinsAverageTD) && (dolphinsAverageTD > 100)) {
     console.log(`The Koalas Average is: ${koalaAverageTD}
     The Dolphins Average is: ${dolphinsAverageTD}
-    The Dolphin's Win!!`)
+    The Dolphin's Win!!`);
 } else {
-    console.log(`Neither Team Wins!`)
-}
+    console.log(`Neither Team Wins!`);
+};
+
+const day = 'monday';
+switch (day) {
+    case 'monday':
+        console.log(`It's Monday.`);
+        break;
+    case 'tuesday':
+        console.log(`It's Tuesday.`);
+        break;
+    case 'wednesday':
+    case 'thursday':
+    case 'friday':
+        console.log(`The remainder of the workweek.`);
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log(`Its the weekend!!`);
+        break;
+    default:
+        console.log(`Not a valid day`);
+        break;
+};
+
+const age = 23;
+// age >= 18 ? console.log(`I like to drink wine`) : console.log(`I like to drink water`);
+
+const drink = age >= 18 ? `wine` : `water`;
+console.log(drink);
+
+// coding challenge #4
+
+const billAmount = 430;
+const tip = (billAmount >= 50 && billAmount <= 300) ? (billAmount * 0.15) : (billAmount * 0.20);
+const totalBill = billAmount + tip;
+console.log(tip);
+console.log(`The bill was ${billAmount}, the tip was ${tip}, and the total bill was ${totalBill}`);
+
 
