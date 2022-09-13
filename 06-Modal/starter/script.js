83f8652247f9f3e72dec87c7119f6e9358dce6bd
Sandeep Mohan <sjmohan@gmail.com>
2022-09-13 16:50:17 -0400

    modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// Get all modals as an array
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let x = 0; x < btnsOpenModal.length; x++) {
+  btnsOpenModal[x].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (evnt) {
+  if (evnt.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
