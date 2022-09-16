c90284b75276018296e4d874059b283c0418bba2
Sandeep Mohan <sjmohan@gmail.com>
2022-09-16 11:24:17 -0400

    data structures and operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,13 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +34,106 @@ const restaurant = {
     },
   },
 };
+
+// sample destructuring
+
+const [a, b, c, d] = restaurant.starterMenu;
+console.log(a, b, c, d);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// default values
+
+const [p, q, r] = [8, 9];
+console.log(p, q, r);
+const [s = 1, t = 1, u = 1] = [8, 9];
+console.log(s, t, u);
+
+// default values - destructuring objects not just arrays. great for api
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating Variables
+let xA = 111;
+let yA = 999;
+const obj = { xA: 23, yA: 7, zA: 14 };
+({ xA, yA } = obj); //notice the entire declaration is wrapped in a parens since we are destructuring an object
+console.log(xA, yA);
+
+// Nested Objects
+
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+const menus = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menus);
+
+// Rest Operator and Parameters
+const [m, n, ...others] = [1, 2, 3, 4, 5];
+console.log(m, n, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(pizza, risotto, otherFood);
+
+// rest operator on objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// Rest operator in functions !! Dynamic parameters
+
+function add(...arr) {
+  let sum = 0;
+  for (let index = 0; index < arr.length; index++) {
+    sum += arr[index];
+  }
+  console.log(sum);
+  return sum;
+}
+const sumall = add(23, 76, 85, 45, 63);
+
+restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
+
+// Short Circuiting
+
+console.log(3 || 'Sandeep');
+console.log('' || 'Sandeep');
+console.log(true || 0);
+console.log(false || 0);
+console.log(undefined || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
