6a98d45510764062527a310cb45c4e97299dd188
Sandeep Mohan <sjmohan@gmail.com>
2022-09-17 20:02:55 -0400

    coding challenge 3 complete

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -464,3 +464,39 @@ console.log([...question]);
 // console.log([...question.entries()]); // same as we convert back to array of arrays
 console.log([...question.keys()]);
 console.log([...question.values()]);
+
+// Coding Challenge 3
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3.
+
+console.log(
+  `An event happened, on average, every ${92 / gameEvents.size} minutes`
+);
+for (const [key, value] of gameEvents) {
+  console.log(
+    key <= 45
+      ? `[FIRST HALF] ${key}: ${value}`
+      : `[SECOND HALF] ${key}: ${value}`
+  );
+}
