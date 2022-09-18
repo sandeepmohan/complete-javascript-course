aba71cea693c4518d88350306f9f3d3fefa942d9
Sandeep Mohan <sjmohan@gmail.com>
2022-09-18 11:52:44 -0400

    Section 9 Complete

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -500,3 +500,132 @@ for (const [key, value] of gameEvents) {
       : `[SECOND HALF] ${key}: ${value}`
   );
 }
+
+// Strings and String Methods
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(plane[3]);
+console.log(...plane);
+console.log('B737'[0]);
+console.log(airline.length);
+console.log('B737'.length);
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7)); //start is inclusive end is exclusive
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+const checkMiddleSeat = function (seat) {
+  // B & E are middle seats
+  console.log(
+    seat.slice(-1) === 'B' || seat.slice(-1) === 'E'
+      ? `${seat} is the Middle Seat`
+      : `${seat} is not in the Middle`
+  );
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3A');
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+let passenger = 'sAnDeEp';
+console.log(passenger);
+passenger = passenger.toLowerCase();
+console.log(passenger);
+passenger = passenger[0].toUpperCase() + passenger.slice(1);
+console.log(passenger);
+
+let passenger2 = 'eLizaBEth';
+passenger2 = passenger2[0].toUpperCase() + passenger2.slice(1).toLowerCase();
+console.log(passenger2);
+
+const announcement =
+  'All Passengers come to boarding door 23. Boarding door 23.';
+console.log(announcement.replace(/door/g, 'gate'));
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  console.log(
+    baggage.includes('knife') || baggage.includes('gun')
+      ? 'You are not allowed on the plane'
+      : 'Welcome Aboard'
+  );
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and gun for protection');
+
+const planename = 'Airbus A320neo';
+console.log(
+  planename.startsWith('Airbus') && planename.endsWith('neo')
+    ? 'Part of the new Airbus fleet'
+    : 'Not part of new Airbus fleet'
+);
+// Coding Challenge 4
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+
+  // Long way - my first attempt but it works
+  const valarray = text.split('\n');
+  const finarray = [];
+  const cleanarray = [];
+  for (const val of valarray) {
+    finarray.push(val.toLowerCase().trim().split('_'));
+  }
+  for (const finval of finarray) {
+    cleanarray.push(
+      `${finval[0]}${finval[1][0].toUpperCase() + finval[1].slice(1)}`
+    );
+  }
+  for (let index = 0; index < cleanarray.length; index++) {
+    const cleanval = cleanarray[index];
+    console.log(cleanval);
+  }
+
+  // right way - shorter and more elegant
+  const rows = text.split('\n');
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLocaleLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+
+// More String methods practice:
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const indFlights = flights.split('+');
+console.log(indFlights);
+const getCode = str => str.slice(0, 3).toUpperCase();
+for (const indflight of indFlights) {
+  const [status, from, to, time] = indflight.split(';');
+  const output = `${status.startsWith('_Delayed') ? '🔴' : ''} ${status
+    .replaceAll('_', ' ')
+    .trim()} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(45);
+  console.log(output);
+}
